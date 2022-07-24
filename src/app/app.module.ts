da2c1cd15fb42b30e241f462150bfb12d0d3e3f6
LukSiman <simanavicius.lukas@gmail.com>
2022-07-24 11:11:56 -0700

    generating pages and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainPageComponent } from './components/main-page/main-page.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ContactPageComponent } from './components/contact-page/contact-page.component';
+import { ProjectPageComponent } from './components/project-page/project-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent,
+    NavbarComponent,
+    FooterComponent,
+    ContactPageComponent,
+    ProjectPageComponent
   ],
   imports: [
     BrowserModule,
