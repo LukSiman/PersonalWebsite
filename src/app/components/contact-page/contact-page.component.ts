7695b747cc57072a81241d7d861d41c3b9796ec5
LukSiman <simanavicius.lukas@gmail.com>
2022-07-30 19:35:41 -0700

    creating the contact form and methods

--- src/app/components/contact-page/contact-page.component.ts
+++ src/app/components/contact-page/contact-page.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { EmailService } from 'src/app/services/email.service';
+import { CustomValidator } from 'src/app/validators/custom-validator';
 
 @Component({
   selector: 'app-contact-page',
@@ -7,9 +10,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactPageComponent implements OnInit {
 
-  constructor() { }
+  formGroup!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder, private emailService: EmailService) { }
 
   ngOnInit(): void {
+    this.formGroup = this.formBuilder.group({
+      fullName: ['', [Validators.required, CustomValidator.notOnlyWhitespace]],
+      email: ['', [Validators.compose([Validators.required, Validators.email])]],
+      comment: ['', [Validators.required, Validators.minLength(10), CustomValidator.notOnlyWhitespace]]
+    });
   }
 
+  get fullName() { return this.formGroup.get('fullName'); }
+  get email() { return this.formGroup.get('email'); }
+  get comment() { return this.formGroup.get('comment'); }
+
+  // submits the message to email service
+  onSubmit(formGroup: FormGroup) {
+    console.log(formGroup)
+    this.emailService.postMessage(formGroup).subscribe(response => {
+      location.href = 'https://mailthis.to/confirm';
+      console.log(response);
+    });
+  }
+
+  // clears the form
+  formCleaner(){
+    this.formGroup.reset();
+  }
 }
